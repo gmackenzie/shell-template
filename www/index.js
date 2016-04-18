39e3bf22a468e59802a4eab5605ab3891e729117
Graham Mackenzie <gmackenzie@commontime.com>
2016-04-18 10:59:23 +0100

    Update index.js

--- www/index.js
+++ www/index.js
@@ -1,87 +1,88 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-	
-	// var timeoutHandle;
-	
-	var retryButton = document.getElementById("retry");		
-	var settingsButton = document.getElementById("settings");	
-	var progressBar = document.getElementById("progress");
-	var status = document.getElementById("status");
-	
-	console.log("Ready");
 
-	plugins.provisioning.registerCacheUpdates(function(data) {
-	    if( data.state == 0 ) {
-	        setProgress(data.detail / 2);
-	    }
-	    if( data.state == 8 ) {
+    // var timeoutHandle;
+
+    var retryButton = document.getElementById("retry");
+    var settingsButton = document.getElementById("settings");
+    var progressBar = document.getElementById("progress");
+    var status = document.getElementById("status");
+
+    console.log("Ready");
+
+    plugins.provisioning.registerCacheUpdates(function(data) {
+        if (data.state == 0) {
+            setProgress(data.detail / 2);
+        }
+        if (data.state == 8) {
             setProgress(50 + (data.detail / 2));
-  	    }
-  	    if( data.state == 4 ) {
-  	        plugins.provisioning.swapCache(function(result) {
-  	            console.log("Swapped");
-  	           	plugins.provisioning.unregisterCacheUpdates(function(data) {
-					startApp();
-  	            }, function() {
-  	            	// Fail
-  	            }, "start");
-  	        }, function() {
-  	            // Fail
-  	        });
-  	    }
-  	    if( data.state == 2 ) {
-  	    	// Have already seen update
-  	    	console.log("Already up to date");
-  	    	startApp();
-  	    }
-		if( data.state == 9 ) {
-  	    	// Have already seen update
-  	    	console.log("Failed");
-  	    	failed();
-  	    }
-	}, function() {
-	    // Fail
-	}, "start");
+        }
+        if (data.state == 4) {
+            plugins.provisioning.swapCache(function(result) {
+                console.log("Swapped");
+                plugins.provisioning.unregisterCacheUpdates(function(data) {
+                    startApp();
+                }, function() {
+                    // Fail
+                }, "start");
+            }, function() {
+                // Fail
+            });
+        }
+        if (data.state == 2) {
+            // Have already seen update
+            console.log("Already up to date");
+            startApp();
+        }
+        if (data.state == 9) {
+            // Have already seen update
+            console.log("Failed");
+            failed();
+        }
+    }, function() {
+        // Fail
+    }, "start");
 
-	plugins.provisioning.start(function(data) {
-		// Success
-	}, function() {
-		// Fail
-	});
-	
-	function failed() {
-		// clearTimeout(timeoutHandle);
+    plugins.provisioning.start(function(data) {
+        // Success
+    }, function() {
+        // Fail
+    });
 
-		status.innerHTML = "Failed to download app";
+    function failed() {
+        // clearTimeout(timeoutHandle);
 
-		showButtons();		
+        status.innerHTML = "Failed to dowload app";
 
-		progressBar.className = progressBar.className.replace(/\bprogress__indeterminate\b/,'');
-		progressBar.MaterialProgress.setProgress(0);
+        showButtons();
 
-		retryButton.removeEventListener("click", retry);
-		retryButton.addEventListener("click", retry);
-		
-		if(plugins.settingsUi != undefined) {		
-			showButtons();
+        progressBar.className = progressBar.className.replace(/\bprogress__indeterminate\b/, '');
+        progressBar.MaterialProgress.setProgress(0);
 
-			settingsButton.removeEventListener("click", settings);
-			settingsButton.addEventListener("click", settings);
-		}
-	}
+        retryButton.removeEventListener("click", retry);
+        retryButton.addEventListener("click", retry);
+
+        if (plugins.settingsUi != undefined) {
+            showButtons();
+
+            settingsButton.removeEventListener("click", settings);
+            settingsButton.addEventListener("click", settings);
+        }
+    }
 
     function settings() {
 
         status.innerHTML = "";
 
         plugins.settingsUi.open(function() {
-        	hideButtons();
-        	plugins.provisioning.start(function(data) {
-        		// Success
-        	}, function() {
-        		// Fail
-        	});
+            hideButtons();
+            progressBar.className = progressBar.className + " progress__indeterminate";
+            plugins.provisioning.start(function(data) {
+                // Success
+            }, function() {
+                // Fail
+            });
         });
     }
 
@@ -90,45 +91,43 @@ function onDeviceReady() {
         status.innerHTML = "";
 
         hideButtons();
-
         progressBar.className = progressBar.className + " progress__indeterminate";
 
         plugins.provisioning.start(function(data) {
-        	// Success
+            // Success
         }, function() {
-        	// Fail
+            // Fail
         });
     }
 
-	function hideButtons() {
-		if(settingsButton.className.indexOf("hidden") < 0 ) {		
-			settingsButton.className = settingsButton.className + " hidden";
-		}
-		if(retryButton.className.indexOf("hidden") < 0 ) {		
-			retryButton.className = retryButton.className + " hidden";
-		}
-	}
+    function hideButtons() {
+        if (settingsButton.className.indexOf("hidden") < 0) {
+            settingsButton.className = settingsButton.className + " hidden";
+        }
+        if (retryButton.className.indexOf("hidden") < 0) {
+            retryButton.className = retryButton.className + " hidden";
+        }
+    }
 
-	function showButtons() {
-		retryButton.className = retryButton.className.replace(/\bhidden\b/,'');		
-		settingsButton.className = settingsButton.className.replace(/\bhidden\b/,'');
-	}
+    function showButtons() {
+        retryButton.className = retryButton.className.replace(/\bhidden\b/, '');
+        settingsButton.className = settingsButton.className.replace(/\bhidden\b/, '');
+    }
 
-	function setProgress(percentage) {
-		progressBar.className = progressBar.className.replace(/\bprogress__indeterminate\b/,'');
-		progressBar.MaterialProgress.setProgress(percentage);
-		//clearTimeout(timeoutHandle);
-		hideButtons();
-	}
+    function setProgress(percentage) {
+        progressBar.className = progressBar.className.replace(/\bprogress__indeterminate\b/, '');
+        progressBar.MaterialProgress.setProgress(percentage);
+        //clearTimeout(timeoutHandle);
+        hideButtons();
+    }
 
-	function startApp() {
-		plugins.provisioning.switchToApp(function(data) {
-			// OK
-		}, function() {
-			// Fail
-		});
-	}
-	
-	// timeoutHandle = setTimeout(failed, 10000);
+    function startApp() {
+        plugins.provisioning.switchToApp(function(data) {
+            // OK
+        }, function() {
+            // Fail
+        });
+    }
+
+    // timeoutHandle = setTimeout(failed, 10000);
 }
-
